c9b96db3bfe62cc7257c3ae762a37477d140cd6b
Joe Black <jblack248@gmail.com>
2013-07-11 23:51:45 -0400

    Separate msm7x30 blobs from mecha and remove extra chromatix libs
    Change-Id: I576f678eebfd9f7b87e910a9eee0f540c420ce9a

--- mecha/mecha-vendor-blobs.mk
+++ mecha/mecha-vendor-blobs.mk
@@ -23,8 +23,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
-    vendor/htc/mecha/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
-    vendor/htc/mecha/proprietary/lib/hw/camera.default.so:obj/lib/hw/camera.default.so \
     vendor/htc/mecha/proprietary/lib/libqc-opt.so:obj/lib/libqc-opt.so \
     vendor/htc/mecha/proprietary/lib/libril.so:obj/lib/libril.so \
     vendor/htc/mecha/proprietary/lib/libv8.so:obj/lib/libv8.so
@@ -32,9 +30,6 @@ PRODUCT_COPY_FILES += \
 # Proprietary blobs
 PRODUCT_COPY_FILES += \
     vendor/htc/mecha/proprietary/bin/snd3254:system/bin/snd3254 \
-    vendor/htc/mecha/proprietary/lib/libgemini.so:system/lib/libgemini.so \
-    vendor/htc/mecha/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
-    vendor/htc/mecha/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
     vendor/htc/mecha/proprietary/bin/akmd:system/bin/akmd \
     vendor/htc/mecha/proprietary/bin/bma150_usr:system/bin/bma150_usr \
     vendor/htc/mecha/proprietary/bin/htc_ebdlogd:system/bin/htc_ebdlogd \
@@ -42,29 +37,8 @@ PRODUCT_COPY_FILES += \
     vendor/htc/mecha/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/htc/mecha/proprietary/bin/awb_camera:system/bin/awb_camera \
     vendor/htc/mecha/proprietary/bin/lsc_camera:system/bin/lsc_camera \
-    vendor/htc/mecha/proprietary/lib/hw/camera.default.so:system/lib/hw/camera.default.so \
-    vendor/htc/mecha/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
-    vendor/htc/mecha/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_ov8810_default_video.so:system/lib/libchromatix_ov8810_default_video.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_ov8810_preview.so:system/lib/libchromatix_ov8810_preview.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_ov8830_default_video.so:system/lib/libchromatix_ov8830_default_video.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_ov8830_hfr.so:system/lib/libchromatix_ov8830_hfr.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_ov8830_preview.so:system/lib/libchromatix_ov8830_preview.so \
     vendor/htc/mecha/proprietary/lib/libchromatix_s5k3h1gx_default_video.so:system/lib/libchromatix_s5k3h1gx_default_video.so \
     vendor/htc/mecha/proprietary/lib/libchromatix_s5k3h1gx_preview.so:system/lib/libchromatix_s5k3h1gx_preview.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k3h2yx_default_video.so:system/lib/libchromatix_s5k3h2yx_default_video.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k3h2yx_hfr.so:system/lib/libchromatix_s5k3h2yx_hfr.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k3h2yx_preview.so:system/lib/libchromatix_s5k3h2yx_preview.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k4e1gx_default_video.so:system/lib/libchromatix_s5k4e1gx_default_video.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k4e1gx_preview.so:system/lib/libchromatix_s5k4e1gx_preview.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k4e5yx_default_video.so:system/lib/libchromatix_s5k4e5yx_default_video.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
-    vendor/htc/mecha/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
-    vendor/htc/mecha/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
-    vendor/htc/mecha/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-    vendor/htc/mecha/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-    vendor/htc/mecha/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
-    vendor/htc/mecha/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
     vendor/htc/mecha/proprietary/bin/btld:system/bin/btld \
     vendor/htc/mecha/proprietary/bin/dmagent:system/bin/dmagent \
     vendor/htc/mecha/proprietary/bin/ipd:system/bin/ipd \
@@ -109,33 +83,5 @@ PRODUCT_COPY_FILES += \
     vendor/htc/mecha/proprietary/lib/libwmsts.so:system/lib/libwmsts.so \
     vendor/htc/mecha/proprietary/lib/libwsp_jni.so:system/lib/libwsp_jni.so \
     vendor/htc/mecha/proprietary/lib/libwsp.so:system/lib/libwsp.so \
-    vendor/htc/mecha/proprietary/lib/libwvm.so:system/lib/libwvm.so \
-    vendor/htc/mecha/proprietary/lib/libWVStreamControlAPI_L3.so:system/lib/libWVStreamControlAPI_L3.so \
-    vendor/htc/mecha/proprietary/lib/libwebkitaccel.so:system/lib/libwebkitaccel.so \
-    vendor/htc/mecha/proprietary/lib/libwvdrm_L3.so:system/lib/libwvdrm_L3.so \
     vendor/htc/mecha/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
-    vendor/htc/mecha/proprietary/lib/libv8.so:system/lib/libv8.so \
-    vendor/htc/mecha/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/vidc_720p_command_control.fw:system/etc/firmware/vidc_720p_command_control.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/vidc_720p_h263_dec_mc.fw:system/etc/firmware/vidc_720p_h263_dec_mc.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/vidc_720p_h264_dec_mc.fw:system/etc/firmware/vidc_720p_h264_dec_mc.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/vidc_720p_h264_enc_mc.fw:system/etc/firmware/vidc_720p_h264_enc_mc.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/vidc_720p_mp4_dec_mc.fw:system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/vidc_720p_mp4_enc_mc.fw:system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/vidc_720p_vc1_dec_mc.fw:system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/mecha/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/htc/mecha/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-    vendor/htc/mecha/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    vendor/htc/mecha/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    vendor/htc/mecha/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    vendor/htc/mecha/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/htc/mecha/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/mecha/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
-    vendor/htc/mecha/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/mecha/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
-    vendor/htc/mecha/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
-    vendor/htc/mecha/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/mecha/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
-    vendor/htc/mecha/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so
+    vendor/htc/mecha/proprietary/lib/libv8.so:system/lib/libv8.so
